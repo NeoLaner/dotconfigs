17783197ecc142ec6ae126a3f97fe94a81af5f41
neolaner <neo.laner.official@gmail.com>
2025-12-21 12:15:35 +0330

    add postgres scripts

---
+++ functional-scripts/postgres/connect-to-postgres.sh
@@ -0,0 +1,11 @@
+# postgres user in db is like root user in linux
+# psql -U postgres
+
+
+psql -U neo -d neos_db
+
+# - `-U neo`: Connect as the user `neo`.
+    
+# - `-d neos_db`: Connect to the **d**atabase named `neos_db`.
+    
+
---
+++ functional-scripts/postgres/start-postgres.sh
@@ -0,0 +1,3 @@
+# sudo -u postgres /usr/local/pgsql/bin/pg_ctl -D /usr/local/pgsql/data -l /usr/local/pgsql/data/logfile start
+## with systemctl
+sudo systemctl start postgresql
